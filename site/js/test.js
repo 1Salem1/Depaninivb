40cffee87985c995a2d373c1a06d15f6bd019768
1Salem1 <Salem.dahmani345@gmail.com>
2021-06-21 10:46:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ site/js/test.js
@@ -0,0 +1,9 @@
+function close_login() {//x eli fil coin ki tenzel aleha tetnaha
+    document.getElementById('form_login').style.display = 'none'
+    return false
+  }
+  
+  function send_login() {//login li fil index tezel aleha tatla3lek
+    document.getElementById('form_login').style.display = 'block'
+  }
+  
